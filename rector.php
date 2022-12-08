ed1776e6f06295e8b62f56e3b84c5ca31237fa73
Matthew Taliancich Jr <Matthew.Taliancich@mheducation.com>
2022-12-08 11:54:36 -0600

    add rector changes

--- rector.php
+++ rector.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php70\Rector\FuncCall\RandomFunctionRector;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
 
@@ -20,18 +22,23 @@ return static function (RectorConfig $rectorConfig): void {
             __DIR__ . '/.idea',
             // Faker wants random to be deterministic, this Rector makes it non-deterministic by replacing it with secure random
             RandomFunctionRector::class,
+            FinalizePublicClassConstantRector::class => [
+                __DIR__ . '/src/Faker/Provider/Person.php',
+            ],
+            FirstClassCallableRector::class => [
+                __DIR__ . '/test/Faker/GeneratorTest.php',
+            ],
         ]
     );
 
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
 
-    //$rectorConfig->parallel(240, 4);
-    $rectorConfig->disableParallel(); // parallel causes freezing, may solve later
+    $rectorConfig->parallel(240, 4);
 
     $rectorConfig->sets(
         [
-            LevelSetList::UP_TO_PHP_74,
-            PHPUnitLevelSetList::UP_TO_PHPUNIT_90
+            LevelSetList::UP_TO_PHP_82,
+            PHPUnitLevelSetList::UP_TO_PHPUNIT_100
         ]
     );
 };
